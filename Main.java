06fcdc0d24feb8eb72ab0764f7fa7f214fd44402
mind_control <mind_control@mail.ru>
2020-03-10 18:26:18 +0300

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPrice = 3000;
+        long miles = ticketPrice / 20;
+        System.out.println(miles);
+    }
+}
